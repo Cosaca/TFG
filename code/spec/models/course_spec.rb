f9953b9373eed655ecfa9bf4153d9f7475bbcd35
Samuel Valcarcel <alu0100831382@ull.edu.es>
2018-06-19 14:35:45 +0100

    Arreglando validaciones y añadiendo pruebas

--- code/spec/models/course_spec.rb
+++ code/spec/models/course_spec.rb
@@ -1,11 +1,12 @@
 require 'rails_helper'
 
-describe Course do
+RSpec.describe Course, :type => :model do
   subject { described_class.new }
 
   it "se comprueba con atributos válidos" do
     subject.title = 'Prueba de título'
     subject.body = 'Prueba de descripción'
+    subject.value_reference = 'Valor de referencia'
     expect(subject).to be_valid
   end
 
@@ -16,5 +17,14 @@ describe Course do
   it "no se valida sin descripción" do
     subject.title = "Prueba de descripción"
     expect(subject).to_not be_valid
+  end
+
+  it "no se valida sin valor de referencia" do
+    subject.value_reference = "Prueba de valor de referencia"
+    expect(subject).to_not be_valid
+  end
+
+  it "tiene uno o más usuarios en las secciones de ese curso" do
+    should have_many(:users).through(:sections)
   end
 end
--- code/spec/models/section_spec.rb
+++ code/spec/models/section_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe Section do
+RSpec.describe Section, :type => :model do
   subject { described_class.new }
 
   it "se comprueba con atributos válidos" do
@@ -17,8 +17,28 @@ describe Section do
     expect(section.macro).to eq :belongs_to 
   end
 
-  it "puede tener uno o más usuarios" do 
-    section = described_class.reflect_on_association(:user)
-    expect(section.macro).to eq :has_many
+  it "debe tener uno o más usuarios" do 
+    should have_many(:users)
+  end
+
+  it "Se pueden añadir nuevos usuarios" do
+    @new_user = User.new(first_name: "Samuel", last_name: "Valcarcel", gender: "Chico", age: "23", username: "cosaca", total_lines: "40", completed_levels: "20", section_id: "1", user_value_reference: "7.5")
+    @new_user.save
+    expect(User.find_by(first_name: "Samuel")).to eq(@new_user)
+  end
+
+  it "Se pueden eliminar usuarios" do
+    @new_user = User.new(first_name: "Samuel", last_name: "Valcarcel", gender: "Chico", age: "23", username: "cosaca", total_lines: "40", completed_levels: "20", section_id: "1", user_value_reference: "7.5")
+    @new_user.destroy
+  end
+
+  it "No se validan los usuarios sin todos los atributos" do
+    @new_user = User.new(first_name: "Nombre")
+    expect(@new_user).to_not be_valid
+  end
+
+  it "La edad debe ser en números" do 
+    @new_user = User.new(age: "Años")
+    expect(@new_user).to_not be_valid
   end
 end
