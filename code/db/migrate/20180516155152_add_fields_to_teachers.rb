d0dc61cb017f9815c274372feecbc9cc3d2c9dfc
Samuel Valcarcel <alu0100831382@ull.edu.es>
2018-06-23 11:36:29 +0100

    Arreglando fallo base de datos

--- code/db/migrate/20180516155152_add_fields_to_teachers.rb
+++
@@ -1,6 +0,0 @@
-class AddFieldsToTeachers < ActiveRecord::Migration[5.2]
-  def change
-    add_column :teachers, :first_name, :string
-    add_column :teachers, :last_name, :string
-  end
-end
--- code/db/schema.rb
+++ code/db/schema.rb
@@ -42,12 +42,14 @@ ActiveRecord::Schema.define(version: 2018_06_11_142756) do
     t.datetime "last_sign_in_at"
     t.string "current_sign_in_ip"
     t.string "last_sign_in_ip"
-    t.string "name"
-    t.string "permission_level"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
     t.string "first_name"
     t.string "last_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["email"], name: "index_teachers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_teachers_on_reset_password_token", unique: true
   end
